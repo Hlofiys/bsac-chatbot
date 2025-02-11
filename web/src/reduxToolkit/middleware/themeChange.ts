19443ad4d79ed80ab08f116f48886a09c098763a
Kirik_10 <kirik.kostya@list.ru>
2025-02-11 23:56:16 +0300

    bugfix/clientRender

--- web/src/reduxToolkit/middleware/themeChange.ts
+++ web/src/reduxToolkit/middleware/themeChange.ts
@@ -1,3 +1,4 @@
+"use client"
 import { Middleware } from "@reduxjs/toolkit";
 import { hydrateTheme } from "../Slices";
 import { IMessage } from "@/components/ui/chatList/messageSection/MessageSection";
