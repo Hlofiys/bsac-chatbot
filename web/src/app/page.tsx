268b9dccd6efb9c752b6b0e2552b0b0cb8aa4034
hlofiys <hlofiys@gmail.com>
2025-02-12 19:19:21 +0300

    fixed build issue

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,47 +1,23 @@
 "use client";
-import { FC, Suspense } from "react";
+import { FC } from "react";
 import dynamic from "next/dynamic";
 import styles from "./styles/Chatbot.module.scss";
 import "@ant-design/v5-patch-for-react-19";
-import loading from "../../public/lottie/loading.json";
 const ChatList = dynamic(() => import("@/components/ui/chatList/ChatList"), {
   ssr: false,
 });
-const Header = dynamic(() => import("@/components/ui/header/Header"), {
-  ssr: false,
-});
-const Footer = dynamic(() => import("@/components/ui/footer/Footer"), {
-  ssr: false,
-});
+import Header from "@/components/ui/header/Header";
+import Footer from "@/components/ui/footer/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Lottie from "lottie-react";
 
 const Chatbot: FC = () => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <div className={styles.container}>
-        <Suspense
-          fallback={
-            <div
-              style={{
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Lottie
-                animationData={loading}
-                style={{ width: 100, height: 100 }}
-              />
-            </div>
-          }
-        >
           <Header />
           <ChatList />
           <Footer />
-        </Suspense>
       </div>
     </QueryClientProvider>
   );
