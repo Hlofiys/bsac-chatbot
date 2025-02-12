1d4312f1b80bee7d9554e71e013b4cbdf1c43476
Kirik_10 <kirik.kostya@list.ru>
2025-02-12 19:03:40 +0300

    bugfix/dynamicImport

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,16 @@
 "use client";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import dynamic from "next/dynamic";
 import styles from "./styles/Chatbot.module.scss";
-import Header from "@/components/ui/header/Header";
 import "@ant-design/v5-patch-for-react-19";
-import Footer from "@/components/ui/footer/Footer";
 import loading from "../../public/lottie/loading.json";
 const ChatList = dynamic(() => import("@/components/ui/chatList/ChatList"), {
-  loading: () => (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Lottie animationData={loading} style={{ width: 100, height: 100 }} />
-    </div>
-  ),
+  ssr: false,
+});
+const Header = dynamic(() => import("@/components/ui/header/Header"), {
+  ssr: false,
+});
+const Footer = dynamic(() => import("@/components/ui/footer/Footer"), {
   ssr: false,
 });
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -29,9 +21,27 @@ const Chatbot: FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <div className={styles.container}>
-        <Header />
-        <ChatList />
-        <Footer />
+        <Suspense
+          fallback={
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Lottie
+                animationData={loading}
+                style={{ width: 100, height: 100 }}
+              />
+            </div>
+          }
+        >
+          <Header />
+          <ChatList />
+          <Footer />
+        </Suspense>
       </div>
     </QueryClientProvider>
   );
